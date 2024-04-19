48d430385104a8569e2cea1f1945a6467b6c6c53
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-19 21:59:37 +0800

    [signup.php] <Decorate> Format HTML elements.

--- public/signup.php
+++ public/signup.php
@@ -9,15 +9,34 @@
 </head>
 
 <body>
-    <form action="./php/process-signup.php" method="post">
-        <lable for="u_name">User Name:</lable>
-        <input type="text" id="u_name" name="u_name">
+    <div class="login-signin-panel">
+        <h1>Sign Up</h1>
 
-        <label for="u_pwd">Password: </label>
-        <input type="text" id="u_pwd" name="u_pwd">
+        <!-- User Submit Form-->
+        <form action="./php/process-signup.php" method="post">
+            <!-- User Name -->
+            <div class="label-and-text-input">
+                <lable for="u_name">User Name:</lable>
+                <input type="text" id="u_name" name="u_name">
+            </div>
 
-        <input type="submit" value="Sign Up"></input>
-    </form>
+            <!-- Password -->
+            <div class="label-and-text-input">
+                <label for="u_pwd">Password: </label>
+                <input type="text" id="u_pwd" name="u_pwd">
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="label-and-text-input">
+                <label for="u_pwd">Password: </label>
+                <input type="text" id="u_pwd" name="u_pwd">
+            </div>
+
+            <!-- Submit Button -->
+            <input type="submit" value="Sign Up"></input>
+        </form>
+    </div>
+
 </body>
 
 </html>
