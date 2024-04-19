8c2926f7fdb2635df16552baf3b875c1b7d724b2
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-20 00:31:11 +0800

    [signin.php] <Decorate> Format HTML elements.

--- public/login.php
+++ public/login.php
@@ -6,14 +6,29 @@
     <title>Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 
 <body>
+    <h1>Sign In</h1>
+
+    <!-- User Submit Form -->
     <form action="./php/process-login.php" method="post">
-        <lable for="uid">User ID:</lable>
-        <input type="text" id="uid" name="uid">
-        <input type="text" id="u_pwd" name="u_pwd">
-        <input type="submit" value="Login"></input>
+
+        <!-- User ID -->
+        <div class="label-and-text-input">
+            <lable for="uid">User ID:</lable>
+            <input type="text" id="uid" name="uid">
+        </div>
+
+        <!-- Password -->
+        <div class="label-and-text-input">
+            <label for="u_pwd">User Password: </label>
+            <input type="text" id="u_pwd" name="u_pwd">
+        </div>
+
+        <!-- Submit Button -->
+        <input type="submit" value="Login" class="submit-btn"></input>
     </form>
 </body>
 
