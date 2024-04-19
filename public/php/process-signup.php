5328f711cf736712c90ed0066fd17ee35a76d512
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-20 01:31:51 +0800

    [process-signup.php] <Fix> Remove manual increase of uid.

--- public/php/process-signup.php
+++ public/php/process-signup.php
@@ -45,37 +45,6 @@ function checkDupUserName($conn, $u_name)
 
 }
 
-/**
- * @param $conn     MySQL Connection
- * @return int|null Latest User ID. If no latest, return null.
- */
-function getLatestUID($conn)
-{
-    $sql = '
-        SELECT u_id FROM qa_user ORDER BY u_id DESC LIMIT 1;
-    ';
-
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        die('Prepared statement error.');
-    }
-
-    // Execute query
-    mysqli_stmt_execute($stmt);
-
-    // bind result to variable $db_latest_UID
-    mysqli_stmt_bind_result($stmt, $db_l_uid);
-
-    // fetch result
-    if (mysqli_stmt_fetch($stmt)) {
-        return $db_l_uid;
-    } else {
-        return null;
-    }
-
-
-}
-
 /*
     --------- 1. Initialize db connection ---------
 */
@@ -90,22 +59,12 @@ if ($haveDupUserName == true) {
     die();
 }
 
-// 1.2 Get latest user ID
-$latestUID = getLatestUID($conn) ? getLatestUID($conn) : 1;
-echo "<br>";
-echo "Latest UID: " . $latestUID;
-
 // 1.3 Hashing password
 $u_pwd_hash = hash('sha256', $u_pwd);
 
-/* 
-    -------- 2. Insert new User into DB -------- 
-*/
-$newUID = $latestUID + 1;
-
 // 2.1 Prepare Statement
 $sql = '
-    INSERT INTO qa_user (u_id, u_name, u_pwd) VALUES (?, ?, ?);
+    INSERT INTO qa_user (u_name, u_pwd) VALUES (?, ?);
 ';
 
 $stmt = mysqli_stmt_init($conn);
@@ -114,7 +73,7 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
 }
 
 // 2.2 Bind parameters
-mysqli_stmt_bind_param($stmt, 'iss', $newUID, $u_name, $u_pwd_hash);
+mysqli_stmt_bind_param($stmt, 'ss', $u_name, $u_pwd_hash);
 
 // 2.3 execute the query
 mysqli_stmt_execute($stmt);
