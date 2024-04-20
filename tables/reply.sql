6cb476cb183f9f3cb4a24bbecf66d471c9a93035
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-20 19:36:45 +0800

    [post.sql, qa_user.sql, reply.sql] <Add> Add dumped table.

--- tables/reply.sql
+++ tables/reply.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2024 at 01:52 PM
+-- Generation Time: Apr 20, 2024 at 01:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,8 +31,17 @@ CREATE TABLE `reply` (
   `r_id` int(11) NOT NULL COMMENT 'Reply ID',
   `p_id` int(11) NOT NULL COMMENT 'Parent post ID',
   `u_id` int(11) NOT NULL,
-  `r_content` varchar(1024) NOT NULL COMMENT 'Reply Content'
+  `r_content` varchar(1024) NOT NULL COMMENT 'Reply Content',
+  `r_create_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reply`
+--
+
+INSERT INTO `reply` (`r_id`, `p_id`, `u_id`, `r_content`, `r_create_time`) VALUES
+(1, 1, 2, 'This is a reply', '2024-04-19 11:56:42'),
+(2, 1, 1, 'This is a reply from Admin.', '2024-04-19 11:57:02');
 
 --
 -- Indexes for dumped tables
@@ -45,6 +54,16 @@ ALTER TABLE `reply`
   ADD PRIMARY KEY (`r_id`),
   ADD KEY `Reply belong to post` (`p_id`),
   ADD KEY `User post reply` (`u_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Reply ID', AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
