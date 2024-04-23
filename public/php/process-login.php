b42ec71b33179a6b4f47958ef375969edc39139f
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-23 21:53:59 +0800

    [process-login.php] <Decorate> Remove redundant comments.

--- public/php/process-login.php
+++ public/php/process-login.php
@@ -22,31 +22,20 @@ if (!mysqli_stmt_prepare($stmt_login, $sql_login)) {
     die('wrong');
 }
 
-// bind variables
+// bind variables, execute query, bind result to db_u_pwd_hash
 mysqli_stmt_bind_param($stmt_login, 's', $u_name);
-
-// execute the query.
 mysqli_stmt_execute($stmt_login);
-
-// bind result to variable $db_u_pwd
 mysqli_stmt_bind_result($stmt_login, $db_u_id, $db_u_pwd_hash);
 
 // fetch result
 if (mysqli_stmt_fetch($stmt_login) && $db_u_pwd_hash == $u_pwd_hash) {
-    echo "Password: " . $u_pwd . "<br>";
-    echo "Login Success!!!";
-
+    // Login Success
     session_start();
     $_SESSION["u_id"] = $db_u_id;
     $_SESSION["u_name"] = $u_name;
 
     header("Location: ../all-posts.php");
 } else {
-    echo "Login Failed <br>";
-    echo "Password Inputted: " . $u_pwd . "<br>";
-    echo "Password Hash: " . $u_pwd_hash . "<br>";
-    echo "Password Hash from DB: " . $db_u_pwd_hash . "<br>";
-
     // Login Failed, password doesn't match.
     header("Location: ../login.php");
 }
