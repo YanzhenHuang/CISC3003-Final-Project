92952290550c22c393b69616904ebe097eca30b2
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-28 13:15:15 +0800

    [all-posts.php, allPostsStyles.css] <Add> Closed question styles.

--- public/all-posts.php
+++ public/all-posts.php
@@ -119,14 +119,27 @@
         while (mysqli_stmt_fetch($stmt_all_posts)) {
             // Format display time
             $p_create_time = getDisplayTimeString($p_create_time);
+            $close_style_tag = '';
+            $p_is_closed == 1 ? $close_style_tag = 'question-closed' : '';
 
             echo '<a href="./post-details.php?post_id=' . $p_id . '">';
+            echo '<div class="question-card content-block ' . $close_style_tag . '" id="p_id-' . $p_id . '">';
 
-            echo '<div class="question-card content-block" id="p_id-' . $p_id . '">';
-            echo '<div class="card-title"><p class="user-name">' . $u_name . '</p> <p class="create-time">' . $p_create_time . '</p></div>';
-            echo '<p class="content">' . $p_content . '</p>';
+            // Question card header
+            echo '<div class="card-title">';
+            echo '<p class="user-name">' . $u_name . '</p> ';
+
+            echo '<div class="question-status-container">';
+            if ($p_is_closed == 1)
+                echo '<p class="closed-tag">Closed</p>';
+            echo '<p class="create-time">' . $p_create_time . '</p>';
             echo '</div>';
 
+            echo '</div>';
+
+            echo '<p class="content">' . $p_content . '</p>';
+
+            echo '</div>';
             echo '</a>';
             // echo $p_id . '  ' . $u_name . '  ' . $p_content . '  ' . $p_create_time . '<br>';
         } ?>
