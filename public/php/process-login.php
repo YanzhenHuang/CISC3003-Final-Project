3e589810d0426d7d8b8cae517045e5a2f881e35f
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-28 20:10:42 +0800

    [process-login.php] <Decorate> Try to add email func but failed.

--- public/php/process-login.php
+++ public/php/process-login.php
@@ -14,7 +14,7 @@ $u_pwd_hash = hash("sha256", $u_pwd);
 
 // Prepare SQL statement
 $sql_login = '
-    SELECT u_id, u_pwd FROM qa_user WHERE u_name = ?;
+    SELECT u_id, u_pwd, u_email FROM qa_user WHERE u_name = ?;
 ';
 
 $stmt_login = mysqli_stmt_init($conn);
@@ -25,7 +25,7 @@ if (!mysqli_stmt_prepare($stmt_login, $sql_login)) {
 // bind variables, execute query, bind result to db_u_pwd_hash
 mysqli_stmt_bind_param($stmt_login, 's', $u_name);
 mysqli_stmt_execute($stmt_login);
-mysqli_stmt_bind_result($stmt_login, $db_u_id, $db_u_pwd_hash);
+mysqli_stmt_bind_result($stmt_login, $db_u_id, $db_u_pwd_hash, $db_u_email);
 
 // fetch result
 if (mysqli_stmt_fetch($stmt_login) && $db_u_pwd_hash == $u_pwd_hash) {
@@ -34,11 +34,30 @@ if (mysqli_stmt_fetch($stmt_login) && $db_u_pwd_hash == $u_pwd_hash) {
     $_SESSION["u_id"] = $db_u_id;
     $_SESSION["u_name"] = $u_name;
 
+    // // Send an email to notify the user
+    // // Set SMTP server and port using ini_set()
+    // ini_set("SMTP", "smtp.163.com");
+    // ini_set("smtp_port", "25");
+    // ini_set("sendmail_from", "huangyanzhen0108@163.com");
+    // ini_set("auth_password", "HCHYGPIQCUGEXCIY");
+
+    // $email_receiver = $db_u_email;
+    // $email_subject = "Recent Login";
+    // $email_message = "You have a recent login!";
+    // $email_headers = 'From: huangyanzhen0108@163.com' . "\r\n";
+
+    // try {
+    //     mail($email_receiver, $email_subject, $email_message, $email_headers);
+    // } catch (Exception $e) {
+    //     echo "<script>console.log('Mail Error " . $e . ".');</script>";
+    // }
+
     header("Location: ../all-posts.php");
 } else {
     // Login Failed, password doesn't match.
     header("Location: ../login.php");
 }
+
 
 mysqli_stmt_close($stmt_login);
 mysqli_close($conn);
