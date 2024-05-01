11f630779df1ea19d1809874d93084c04befb54c
Yanzhen Huang <huangyanzhen0108@163.com>
2024-05-01 17:15:40 +0800

    [process-validate-account.php] <Decorate> Add useful comments.

--- public/php/process-validate-account.php
+++ public/php/process-validate-account.php
@@ -24,17 +24,21 @@ mysqli_stmt_bind_result($stmt_db_u_token_hash, $db_u_token_hash);
 
 $inputToken_hash = hash('sha256', $inputToken);
 
-// Validations
+// Validation
+
+// No user token or doesn't match
 if (!mysqli_stmt_fetch($stmt_db_u_token_hash) || $inputToken_hash != $db_u_token_hash) {
     header("Location: ../validate-account.php");
     die();
 }
 
+// Matches
 mysqli_stmt_close($stmt_db_u_token_hash);
 mysqli_close($conn);
 
+// Set user valid
 $conn = initConnection($host, $username, $password, $dbname);
-
 db_validateUser($conn, $validate_uid);
 
+// Redirect
 header("Location: ../all-Posts.php");
