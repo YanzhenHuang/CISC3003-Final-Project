7945d592a56b298cb614e9bb9dba45b003e0a23a
Yanzhen Huang <huangyanzhen0108@163.com>
2024-05-13 11:29:35 +0800

    [send-email.php] <Fix> Fixed the email security issue.

--- public/php/utils/send-email.php
+++ public/php/utils/send-email.php
@@ -5,6 +5,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
+
 /**
  * Send an email to the target address.
  * @param string $targetAddr  The target address to send the email to.
@@ -12,27 +13,37 @@ require 'phpmailer/src/SMTP.php';
  * @param string $body  The body of the email.
  */
 
+/**
+ *  You need to add your own SMTP server credentials.
+ */
+
 function sendEmail($targetAddr, $subject, $body)
 {
     if ($targetAddr == null) {
         return;
     }
+
     $mail = new PHPMailer(true);
 
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'huangyanzhen0108@gmail.com';
-    $mail->Password = 'tuxpjsscuffdzcxb';
+    $mail->Username = '';   /* SMTP User Name */
+    $mail->Password = '';   /* SMTP Password (Token) */
+
+    if ($mail->Username == '' || $mail->Password == '') {
+        return;
+    }
+
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
-
-    $mail->setFrom('huangyanzhen0108@gmail.com');
+    $mail->setFrom('');     /* SMTP Mail Address */
     $mail->addAddress($targetAddr);
     $mail->isHTML(true);
 
     $mail->Subject = $subject;
     $mail->Body = $body;
+
 
     $mail->send();
 }
